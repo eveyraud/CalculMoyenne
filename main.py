a4d9ec0e4bf3cb3a3b762395e375bd26f5799f8b
eveyraud <118845032+eveyraud@users.noreply.github.com>
2023-01-31 10:52:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from objet import BlocNote   
-
+from objet import BlocNote
 strSrc = ""
 with open("src.txt", "r", encoding="UTF-8") as fichierSrc:
     for ligne in fichierSrc:
@@ -8,6 +7,7 @@ with open("src.txt", "r", encoding="UTF-8") as fichierSrc:
 def sepBloc(strSrc):
     tabSrc = strSrc.split("<tr>")
     for i in range(4):
+        
         del tabSrc[0]
     del tabSrc[-1]
     return tabSrc
@@ -40,16 +40,37 @@ def calculMoyenne(UE):
         notes += float(i.getNote()) * float(i.getCoeff())
     return notes/coeff
 
-print("UE1:", round(calculMoyenne(sepUE(sepBloc(strSrc))['UE1']), 2))
-print("UE2:", round(calculMoyenne(sepUE(sepBloc(strSrc))['UE2']), 2))
-print("UE3:", round(calculMoyenne(sepUE(sepBloc(strSrc))['UE3']), 2))
+def calculMoyenneClass(UE):
+    coeff = 0
+    notes = 0
+    for i in UE:
+        coeff += float(i.getCoeff())
+        notes += float(i.getNoteClass()) * float(i.getCoeff())
+    return notes/coeff
 
+reponse = int(input('''1) Moyenne de toutes les UE;
+2) Compétences en dessous de la moyenne;
+3) Moyenne de classe.
+'''))
+UE1 = round(calculMoyenne(sepUE(sepBloc(strSrc))['UE1']), 2)
+UE2 = round(calculMoyenne(sepUE(sepBloc(strSrc))['UE2']), 2)
+UE3 = round(calculMoyenne(sepUE(sepBloc(strSrc))['UE3']), 2)
+UE1Class = round(calculMoyenneClass(sepUE(sepBloc(strSrc))['UE1']), 2)
+UE2Class = round(calculMoyenneClass(sepUE(sepBloc(strSrc))['UE2']), 2)
+UE3Class = round(calculMoyenneClass(sepUE(sepBloc(strSrc))['UE3']), 2)
+if reponse == 1:
+    print("UE1:", UE1)
+    print("UE2:", UE2)
+    print("UE3:", UE3)
+    print('moyenne generale', round((UE1 + UE2 + UE3)/3, 2))
+elif reponse == 2:
+    for x, y in sepUE(sepBloc(strSrc)).items():
+        for i in y:
+            if float(i.getNote()) < 10:
+                print(x, " : ",i.getNom(), " (", i.getNote(), ")", sep="")
 
-
-
-
-
-
-
-
-
+elif reponse == 3:
+    print("UE1:", UE1Class)
+    print("UE2:", UE2Class)
+    print("UE3:", UE3Class)
+    print('moyenne generale', round((UE1Class + UE2Class + UE3Class)/3, 2))
--- objet.py
+++ objet.py
@@ -3,11 +3,18 @@ class BlocNote:
         def recupNoteCoeffNom(bloc):
             posCoeff = bloc.find('<td class="text_align_center">')+30
             coeff = bloc[posCoeff:bloc.find("<", posCoeff)]
+            
             posNote = bloc.find('<td class="vert text_align_center">')+35
             note = bloc[posNote:bloc.find("<", posNote)]
+            
             posNom = bloc.find('<td colspan="3" class="vert">')+29
             nom = bloc[posNom:bloc.find("<", posNom)]
-            return(note, coeff, nom.strip("\\"))
+            
+            posMoyClass = bloc.find('<th class="text_align_center">')+30
+            noteClass = bloc[posMoyClass:bloc.find("<", posMoyClass)]
+            
+            return(note, coeff, nom.strip("\\"), noteClass)
+        
         self.bloc = bloc
         self.noteCoeffNom = recupNoteCoeffNom(bloc)
         
@@ -19,3 +26,6 @@ class BlocNote:
     
     def getNom(self):
         return self.noteCoeffNom[2]
+    
+    def getNoteClass(self):
+        return self.noteCoeffNom[3]
